9f206701da107056bdd959593482566d84ece815
Idanise <daniseiheme@gmail.com>
2022-04-27 01:08:45 +0100

    add marker

--- script.js
+++ script.js
@@ -18,6 +18,32 @@ if (navigator.geolocation)
       const { longitude } = position.coords
       console.log(`https://www.google.pt/maps/@${latitude},${longitude}`)
       console.log(position)
+
+      const coords = [latitude, longitude]
+
+      const map = L.map('map').setView(coords, 14)
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map)
+
+      map.on('click', function (mapEvent) {
+        form.classList.remove('hidden')
+        // const { lat, lng } = mapEvent.latlng
+
+        // L.marker([lat, lng])
+        //   .addTo(map)
+        //   .bindPopup(L.popup({
+        //     maxWidth: 250,
+        //     minWidth: 100,
+        //     autoClose: false,
+        //     closeOnClick: false,
+        //     className: 'running-popup'
+        //   }))
+        //   .setPopupContent('Workout')
+        //   .openPopup()
+      })
     },
     function () {
       alert('could not get current location')
