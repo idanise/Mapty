cb1f048e32d5aad3fea8946ce8f4283cff5950de
Idanise <daniseiheme@gmail.com>
2022-04-28 16:06:58 +0100

    move to marker on click

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const inputElevation = document.querySelector('.form__input--elevation')
 class Workout {
   date = new Date()
   id = (Date.now() + '').slice(-10)
+  clicks = 0
 
   constructor (coords, distance, duration) {
     this.coords = coords // [lat, lng]
@@ -25,6 +26,10 @@ class Workout {
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
       months[this.date.getMonth()]
     } ${this.date.getDate()}`
+  }
+
+  click () {
+    this.clicks++
   }
 }
 
@@ -69,6 +74,7 @@ console.log(run1, cyc1)
 // APPLICATION ARCHITECTURE
 class App {
   #map
+  #mapZoomLevel = 15
   #mapEvent
   #workouts = []
 
@@ -77,6 +83,8 @@ class App {
     form.addEventListener('submit', this._newWorkout.bind(this))
 
     inputType.addEventListener('change', this._toggleElevationField)
+
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this))
   }
 
   _getPosition () {
@@ -97,7 +105,7 @@ class App {
 
     const coords = [latitude, longitude]
 
-    this.#map = L.map('map').setView(coords, 14)
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel)
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
@@ -184,9 +192,12 @@ class App {
     //Hide form and clear input fields
     this._hideForm()
 
-    //clear input fields
-    inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value =
-      ''
+    // //clear input fields
+    // inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value =
+    //   ''
+
+    //set local storage to all workouts
+    this._setLocalStorage()
   }
 
   _renderWorkoutMarker (workout) {
@@ -255,6 +266,30 @@ class App {
     </li>`
 
     form.insertAdjacentHTML('afterend', html)
+  }
+
+  _moveToPopup (e) {
+    const workoutEl = e.target.closest('.workout')
+
+    if (!workoutEl) return
+
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    )
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1
+      }
+    })
+
+    // using the public interface
+    workout.click()
+  }
+
+  _setLocalStorage () {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts))
   }
 }
 
